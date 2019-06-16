e424e5c50fc1181f3a0eb405a77e7cebd6173519
ArthurUFelix <arthurufelix@gmail.com>
2019-06-16 13:29:54 -0300

    Alterações na loja e validação do CEP aparentemente totalmente implementada

--- js/loja.js
+++ js/loja.js
@@ -6,18 +6,18 @@ var banco = getBanco();
 $(document).ready(function() {
     let dadosLoja = banco[0].loja[0];
 
-    $('#lojaName').val(dadosLoja.nome);
-    $('#lojaCnpj').val(dadosLoja.cnpj);
-    $('#lojaCEP').val(dadosLoja.cep);
-    $('#lojaLog').val(dadosLoja.logradouro);
-    $('#lojaNumero').val(dadosLoja.numero);
-    $('#lojaComp').val(dadosLoja.complement);
-    $('#lojaBairro').val(dadosLoja.bairro);
-    $('#lojaCidade').val(dadosLoja.cidade);
-    $('#lojaEstado').val(dadosLoja.estado);
-    $('#lojaPhone').val(dadosLoja.telefone);
-    $('#lojaEmail').val(dadosLoja.email);
-    $('#lojaUrl').val(dadosLoja.url);
+    $('#lojaName').val(dadosLoja.nome).trigger('focus');
+    $('#lojaCnpj').val(dadosLoja.cnpj).trigger('focus');
+    $('#lojaCEP').val(dadosLoja.cep).trigger('focus');
+    $('#lojaLog').val(dadosLoja.logradouro).trigger('focus');
+    $('#lojaNumero').val(dadosLoja.numero).trigger('focus');
+    $('#lojaComp').val(dadosLoja.complement).trigger('focus');
+    $('#lojaBairro').val(dadosLoja.bairro).trigger('focus');
+    $('#lojaCidade').val(dadosLoja.cidade).trigger('focus');
+    $('#lojaEstado').val(dadosLoja.estado).trigger('focus');
+    $('#lojaPhone').val(dadosLoja.telefone).trigger('focus');
+    $('#lojaEmail').val(dadosLoja.email).trigger('focus');
+    $('#lojaUrl').val(dadosLoja.url).trigger('focus');
 });
 
 /**************
@@ -40,59 +40,62 @@ $(document).ready(function() {
     $('#edit-loja-form').submit(function(event){
         event.preventDefault();
 
-        // Se o CEP for válido
-        if($('#lojaCEP').hasClass('ready-to-send')) {
-            if($('#edit-loja-form').is(':valid')) {
-                // formata os dados e insere no banco
-                let dados = {
-                    nome: $('#lojaName').val(),
-                    cnpj: $('#lojaCnpj').val(),
-                    cep: $('#lojaCEP').val(),
-                    logradouro: $('#lojaLog').val(),
-                    numero: $('#lojaNumero').val(),
-                    complement: $('#lojaComp').val(),
-                    bairro: $('#lojaBairro').val(),
-                    cidade: $('#lojaCidade').val(),
-                    estado: $('#lojaEstado').val(),
-                    telefone: $('#lojaPhone').val(),
-                    email: $('#lojaEmail').val(),
-                    url: $('#lojaUrl').val()
-                };
-        
-                // Atualiza os dados da loja, estando eles vazios ou n
-                bancoUpdate("loja", 0, dados);
-        
-                // redireciona o usuário para a tela de estoque
-                window.location = window.location.href.replace("edit", "index");
-            }
-        } else {
-            $('lojaCEP').prop(':invalid')
+        if($('#edit-loja-form').is(':valid')) {
+            // formata os dados e insere no banco
+            let dados = {
+                nome: $('#lojaName').val(),
+                cnpj: $('#lojaCnpj').val(),
+                cep: $('#lojaCEP').val(),
+                logradouro: $('#lojaLog').val(),
+                numero: $('#lojaNumero').val(),
+                complement: $('#lojaComp').val(),
+                bairro: $('#lojaBairro').val(),
+                cidade: $('#lojaCidade').val(),
+                estado: $('#lojaEstado').val(),
+                telefone: $('#lojaPhone').val(),
+                email: $('#lojaEmail').val(),
+                url: $('#lojaUrl').val()
+            };
+    
+            // Atualiza os dados da loja, estando eles vazios ou n
+            bancoUpdate("loja", 0, dados);
+    
+            // redireciona o usuário para a tela de estoque
+            window.location = window.location.href.replace("edit", "index");
         }
     });
 
     // Monitora o input de CEP para buscar o endereço
     $('#lojaCEP').keyup(function() {
         // Remove o erro
-        $(this).removeClass('is-invalid is-valid valid-to-send');
+        $(this).removeClass('is-invalid is-valid');
+        $(this).removeAttr('pattern');
 
         let cep = $(this).val();
 
         if(cep.length == 8) {
             $.get( "https://viacep.com.br/ws/"+cep+"/json/", function( data ) {
-                console.log(data);
                 if(data.erro == true) {
                     $('#lojaCEP').addClass('is-invalid');
-                } else {
-                    $('#lojaCEP').addClass('is-valid valid-to-send');
+                    $(this).attr('pattern', 'blocked');
 
-                    $('#lojaLog').val(data.logradouro);
-                    $('#lojaBairro').val(data.bairro);
-                    $('#lojaCidade').val(data.localidade);
-                    $('#lojaEstado').val(data.uf);
+                    return false
+                } else {
+                    $('#lojaCEP').addClass('is-valid');
+
+                    $('#lojaLog').val(data.logradouro).trigger('focus');
+                    $('#lojaBairro').val(data.bairro).trigger('focus');
+                    $('#lojaCidade').val(data.localidade).trigger('focus');
+                    $('#lojaEstado').val(data.uf).trigger('focus');
+
+                    return true
                 }
             });
         } else {
             $(this).addClass('is-invalid');
+            $(this).attr('pattern', 'blocked');
+
+            return false
         }
     });
 });
